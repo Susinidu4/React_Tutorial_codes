89fdb8fd36d0dd285ea27a97ae9223aad6a8d96e
Susinidu4 <155086679+Susinidu4@users.noreply.github.com>
2024-12-11 02:12:18 +0530

    UseEffect Example

--- src/App.js
+++ src/App.js
@@ -3,36 +3,112 @@ import './App.css';
 import { Unit } from "./Components/Unit";
 import Header from "./Components/Header";
 
+//EX:- useEfect
+
 const App = () => {
 
-  // const [imageURL,setImaageURL] = useState("")
-  // const [name,setName] = useState("")
-  // const [city,setCity] = useState("")
-  // const [position,setPosition] = useState("")
+  const [apiId,setApiId] = useState('1');
+  const [search,setSearch] = useState(0)
+  const [data, setData] = useState({})
+  // console.log(apiId);
+  // console.log(data);
 
-  //## we can use one useStates for above 4 like below
-  const [inputData,setInputData] = useState({
-    imageURL:'',
-    name:'',
-    city:'',
-    position:'',
-  })
+  useEffect (() => {
+    console.log("useEffect running..");
 
-  const [myData, setMyData] = useState ([]);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const apiCall = async()=>{
+      const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${apiId}`)
+      const data = await res.json()
 
-  //console.log(myData);
+      if(data){
+        setData(data)
+      }
+    }
+
+    if(apiId.length>0 && Number(apiId)>0 && Number(apiId)<=100){
+      console.log("useEffect if condition..");
+
+      apiCall()
+    }
+
+    return () => {
+      console.log("clean up");
+      apiCall();
+    }
+
+  }, [search]);
+
+  return(
+    <div>
+      <input 
+      type="text" 
+      value={apiId} 
+      onChange={(e)=>{setApiId(e.target.value)}} 
+      placeholder="enter id"/>
+
+      <button onClick={()=>setSearch(pre=>pre===0?1:0)}>
+        Serch
+      </button>
+      
+      {data&&(<div>
+        <h2>{data.title}</h2>
+        <p>{data.body}</p>
+      </div>)}
+    </div>
+  )
+}
+
+export default App;
 
 
 
-//## useEffect
+// const App = () => {
 
-// useEffect(()=> {
-//   console.log("use effect calling..");
-// },[inputData.name]);
+//   // const [imageURL,setImaageURL] = useState("")
+//   // const [name,setName] = useState("")
+//   // const [city,setCity] = useState("")
+//   // const [position,setPosition] = useState("")
+
+//   //## we can use one useStates for above 4 like below
+//   const [inputData,setInputData] = useState({
+//     imageURL:'',
+//     name:'',
+//     city:'',
+//     position:'',
+//   })
+
+//   const [myData, setMyData] = useState ([]);
+//   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+//   //console.log(myData);
+
+
+
+// //## useEffect
+
+// // useEffect(()=> {
+// //   console.log("use effect calling..");
+// // },[inputData.name]);
+
+// // useEffect(()=>{
+// //   const changeEindowWidth = () => {
+// //     setWindowWidth(window.innerWidth);
+// //   };
+
+// //   window.addEventListener('resize',()=>{
+// //     setWindowWidth(window.innerWidth);
+// //   });
+
+// //   console.log("use effect calling...");
+
+// // }, [inputData.name])
+
+
+
+// //## useEffect clean up process
 
 // useEffect(()=>{
-//   const changeEindowWidth = () => {
+//   const changeWindowWidth = () => {
 //     setWindowWidth(window.innerWidth);
 //   };
 
@@ -42,144 +118,127 @@ const App = () => {
 
 //   console.log("use effect calling...");
 
+//   return () => {
+//     console.log('use effect cleanup fuction running..')
+//     window.removeEventListener("resize", changeWindowWidth)
+//   }
 // }, [inputData.name])
 
 
+//   return(
+//     <Fragment>
+//     <Header/>
+//     <div className="main_containar">
+//       <h1>{windowWidth}</h1>
+//       <div className="main_left">
 
-//## useEffect clean up process
-
-useEffect(()=>{
-  const changeWindowWidth = () => {
-    setWindowWidth(window.innerWidth);
-  };
-
-  window.addEventListener('resize',()=>{
-    setWindowWidth(window.innerWidth);
-  });
-
-  console.log("use effect calling...");
-
-  return () => {
-    console.log('use effect cleanup fuction running..')
-    window.removeEventListener("resize", changeWindowWidth)
-  }
-}, [inputData.name])
-
-
-  return(
-    <Fragment>
-    <Header/>
-    <div className="main_containar">
-      <h1>{windowWidth}</h1>
-      <div className="main_left">
-
-        {/* <input type='text' value={imageURL} onChange={(e)=>{
-          e.preventDefault()
-          setImaageURL(e.target.value)
-        }}/>
-        <input type='text' value={name} onChange={(e)=>{
-          e.preventDefault()
-          setName(e.target.value)
-        }}/>
-        <input type='text' value={city} onChange={(e)=>{
-          e.preventDefault()
-          setCity(e.target.value)
-        }}/>
-        <input type='text' value={position} onChange={(e)=>{
-          e.preventDefault()
-          setPosition(e.target.value)
-        }}/> */}
+//         {/* <input type='text' value={imageURL} onChange={(e)=>{
+//           e.preventDefault()
+//           setImaageURL(e.target.value)
+//         }}/>
+//         <input type='text' value={name} onChange={(e)=>{
+//           e.preventDefault()
+//           setName(e.target.value)
+//         }}/>
+//         <input type='text' value={city} onChange={(e)=>{
+//           e.preventDefault()
+//           setCity(e.target.value)
+//         }}/>
+//         <input type='text' value={position} onChange={(e)=>{
+//           e.preventDefault()
+//           setPosition(e.target.value)
+//         }}/> */}
 
 
-        <input type='text' value={inputData.imageURL} placeholder='Image URL' onChange={(e)=>{
-          e.preventDefault()
-          setInputData(preInputData => ({
-            ...preInputData,
-            imageURL:e.target.value
-          }))
-        }}/>
-        <input type='text' value={inputData.name} placeholder='Your Name' onChange={(e)=>{
-          e.preventDefault()
-          setInputData(preInputData => ({
-            ...preInputData,
-            name:e.target.value
-          }))
-        }}/>
-        <input type='text' value={inputData.city} placeholder='Your City' onChange={(e)=>{
-          e.preventDefault()
-          setInputData(preInputData => ({
-            ...preInputData,
-            city:e.target.value
-          }))
-        }}/>
-        <input type='text' value={inputData.position} placeholder='Your Position' onChange={(e)=>{
-          e.preventDefault()
-          setInputData(preInputData => ({
-            ...preInputData,
-            position:e.target.value
-          }))
-        }}/>
+//         <input type='text' value={inputData.imageURL} placeholder='Image URL' onChange={(e)=>{
+//           e.preventDefault()
+//           setInputData(preInputData => ({
+//             ...preInputData,
+//             imageURL:e.target.value
+//           }))
+//         }}/>
+//         <input type='text' value={inputData.name} placeholder='Your Name' onChange={(e)=>{
+//           e.preventDefault()
+//           setInputData(preInputData => ({
+//             ...preInputData,
+//             name:e.target.value
+//           }))
+//         }}/>
+//         <input type='text' value={inputData.city} placeholder='Your City' onChange={(e)=>{
+//           e.preventDefault()
+//           setInputData(preInputData => ({
+//             ...preInputData,
+//             city:e.target.value
+//           }))
+//         }}/>
+//         <input type='text' value={inputData.position} placeholder='Your Position' onChange={(e)=>{
+//           e.preventDefault()
+//           setInputData(preInputData => ({
+//             ...preInputData,
+//             position:e.target.value
+//           }))
+//         }}/>
 
 
 
-        <button onClick={()=>
-          {
-            // setMyData(pre=>{
-            //   return [...pre,{
-            //     image: imageURL,
-            //     name,
-            //     city,
-            //     position,
-            //   }]
-            // });
+//         <button onClick={()=>
+//           {
+//             // setMyData(pre=>{
+//             //   return [...pre,{
+//             //     image: imageURL,
+//             //     name,
+//             //     city,
+//             //     position,
+//             //   }]
+//             // });
 
-            //# previuos value handling (direct return)
-            setMyData((pre)=>[
-              ...pre,
-              {
-                image: inputData.imageURL,
-                name: inputData.name,
-                city: inputData.city,
-                position : inputData.position,
-              },
-            ]);
+//             //# previuos value handling (direct return)
+//             setMyData((pre)=>[
+//               ...pre,
+//               {
+//                 image: inputData.imageURL,
+//                 name: inputData.name,
+//                 city: inputData.city,
+//                 position : inputData.position,
+//               },
+//             ]);
 
-            setInputData((pre)=>{
-              if(pre.imageURL.length>0){
-                return {
-                  ...pre,
-                  imageURL: ''
-                };
-              } else {
-               return pre;
-              }
-            });
+//             setInputData((pre)=>{
+//               if(pre.imageURL.length>0){
+//                 return {
+//                   ...pre,
+//                   imageURL: ''
+//                 };
+//               } else {
+//                return pre;
+//               }
+//             });
             
-            setInputData((prename)=>prename.name.includeslength>0?({
-              ...prename,
-              name:'',
-            }):(prename));
+//             setInputData((prename)=>prename.name.includeslength>0?({
+//               ...prename,
+//               name:'',
+//             }):(prename));
             
-            setInputData((precity)=>precity.city.length>0?({
-              ...precity,
-              city: '',
-            }):(precity));
+//             setInputData((precity)=>precity.city.length>0?({
+//               ...precity,
+//               city: '',
+//             }):(precity));
             
-            setInputData((preposi)=>preposi.position.length>0?({
-              ...preposi,
-              position: '',
-            }):(preposi));
+//             setInputData((preposi)=>preposi.position.length>0?({
+//               ...preposi,
+//               position: '',
+//             }):(preposi));
 
-          }
-        }>Submit
-        </button>
+//           }
+//         }>Submit
+//         </button>
 
-      </div>
-      <div className="main_right">
-      {myData?.map(({image,name,city,position},index) => <Unit image={image} name={name} city={city} position={position} key={index}/>)}
-      </div>
-    </div>
-    </Fragment>
-  )}
+//       </div>
+//       <div className="main_right">
+//       {myData?.map(({image,name,city,position},index) => <Unit image={image} name={name} city={city} position={position} key={index}/>)}
+//       </div>
+//     </div>
+//     </Fragment>
+//   )}
  
-export default App;
+// export default App;
